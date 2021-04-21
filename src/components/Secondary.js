2a3991b4cf185e81da365d77308df3b5f2e4b4ff
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-21 02:52:16 +0200

    s

---
+++ src/components/Secondary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {Container} from 'react-bootstrap';
+
+const Secondary = () => {
+  return (
+    <Container>
+      <section className="secondary">
+        <div className="secondary__left">
+            <h3 style={{borderBottom: '2px solid #54483A', textAlign: 'center'}}>NAJNOVIJE</h3>
+        </div>
+
+        <div className="secondary-right">
+
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default Secondary;
