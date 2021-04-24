46d8683bba7a4e30f3a427fcb7066f99954e378f
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-24 15:07:42 +0200

    adding responsovie images for secondary section

--- src/components/Secondary.js
+++ src/components/Secondary.js
@@ -1,16 +1,17 @@
 import React from 'react';
-
 import {Container} from 'react-bootstrap';
-
+ 
+import SecondaryLeft1 from '../assets/images/secondaryleft-1.jpg';
 const Secondary = () => {
   return (
     <Container>
       <section className="secondary">
         <div className="secondary__left">
-            <h3 style={{borderBottom: '2px solid #54483A', textAlign: 'center'}}>NAJNOVIJE</h3>
+            <h3 style={{borderBottom: '2px solid #54483A', textAlign: 'center', fontWeight: '700', padding: '1.5px 3px 3px', backgroundColor: '#1888ff',}}>NAJNOVIJE</h3>
+           <img src={SecondaryLeft1} alt="sandzak vijesti 1" className="secondary__left-img--1" />
         </div>
 
-        <div className="secondary-right">
+        <div className="secondary__right">
 
         </div>
       </section>
