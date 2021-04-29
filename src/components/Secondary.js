776589c0e63aa63d2d2fb045e36b085fb6c03f02
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-29 16:09:18 +0200

    working on right part of secondary section

--- src/components/Secondary.js
+++ src/components/Secondary.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
- 
-import SecondaryLeft1 from '../assets/images/secondaryleft-1.jpg';
 
+import SecondaryLeft1 from '../assets/images/secondaryleft-1.jpg';
+import SecondaryRight1 from '../assets/images/secondaryright-1.jpg';
 
 const Secondary = () => {
   // const state = React.useState('')
-  // const currentState = state[0]; 
+  // const currentState = state[0];
   // const setStaste = state[1]; //function to update the state
 
   return (
     <Container>
       <section className="secondary">
         <div className="secondary__left">
-            <h3 style={{borderBottom: '2px solid #54483A', textAlign: 'center', fontWeight: '700', padding: '1.5px 3px 3px', backgroundColor: '#1888ff',}}>NAJNOVIJE</h3>
-            <div className="secondary__left-big">
-           <img src={SecondaryLeft1} alt="sandzak vijesti 1" className="secondary__left-img--1" />
-           <div className="secondary__left-big--description">
-           <h3>TEST TEST TEST TEST TEST TEST TEST</h3>
-           <p>27. April 2021</p>
-           <p>short description</p>
-           </div>
-           </div>
+          <h3
+            style={{
+              borderBottom: '2px solid #54483A',
+              textAlign: 'center',
+              fontWeight: '700',
+              padding: '1.5px 3px 3px',
+              backgroundColor: '#1888ff',
+            }}
+          >
+            NAJNOVIJE
+          </h3>
+          <div className="secondary__left-big">
+            <img
+              src={SecondaryLeft1}
+              alt="sandzak vijesti"
+              className="secondary__left-img--1"
+            />
+            <div className="secondary__left-big--description">
+              <h3>TEST TEST TEST TEST TEST TEST TEST</h3>
+              <p>27. April 2021</p>
+              <p>short description</p>
+            </div>
+          </div>
         </div>
 
         <div className="secondary__right">
-
+          <img
+            src={SecondaryRight1}
+            alt="sandzak vijesti"
+            className="secondary__right-img"
+          />
+          <div className="secondary__right-description">
+            <h3>TEST TEST TEST TEST TEST .. TEST TEST...</h3>
+            <p>29. April 2021</p>
+          </div>
         </div>
       </section>
     </Container>
@@ -33,6 +55,3 @@ const Secondary = () => {
 };
 
 export default Secondary;
-
-
-
